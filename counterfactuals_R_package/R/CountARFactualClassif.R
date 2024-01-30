5980f0744b1b16cfa497eba1c4da4852559dd6a0
susanne-207 <dandl.susanne@gmail.com>
2024-01-30 18:23:02 +0100

    add documentation + move example to inst/

--- counterfactuals_R_package/R/CountARFactualClassif.R
+++ counterfactuals_R_package/R/CountARFactualClassif.R
@@ -1,9 +1,67 @@
-
+#' CountARFactual (ARF-based Counterfactuals) for Classification Tasks
+#' 
+#' @description 
+#' CountARFactuals generates counterfactuals based on the adversarial random forests introduced in Watson et. al 2023
+#' 
+#' @references 
+#'
+#' Watson, D. S., Blesch, K., Kapar, J., and Wright, M. N. (2023). Adversarial
+#' random forests for density estimation and generative modeling. In Proceedings
+#' of the 26th International Conference on Artificial Intelligence and Statistics, 
+#' pages 5357–5375. PMLR.
+#' 
+#' @examples 
+#' if (require("randomForest")) {
+#'  \donttest{
+#'   # Train a model
+#'   rf = randomForest(Species ~ ., data = iris)
+#'   # Create a predictor object
+#'   predictor = iml::Predictor$new(rf, type = "prob")
+#'   # Find counterfactuals for x_interest
+#'   arf_classif = CountARFactualClassif$new(predictor)
+#'  
+#'   cfactuals = arf_classif$find_counterfactuals(
+#'     x_interest = iris[150L, ], desired_class = "versicolor", desired_prob = c(0.5, 1)
+#'   )
+#'   # Print the counterfactuals
+#'   cfactuals$data
+#'   # Plot evolution of hypervolume and mean and minimum objective values
+#'   cfactuals$evaluate_set()
+#'   cfactuals$plot_parallel()
+#'   }
+#' }
+#' 
+#' @export
 CountARFactualClassif = R6::R6Class("CountARFactualClassif", 
   inherit = CounterfactualMethodClassif,
   
   public = list(
-    initialize = function(predictor, max_feats_to_change = 1L, n_synth = 10L, feature_selector = "random_importance", importance_method = "fastshap") { 
+    #' @description Create a new `MOCClassif` object.
+    #' @template predictor
+    #' @param max_feats_to_change (`numeric(1)`)\cr  
+    #' The maximum number of features allowed to be altered (default 1L).
+    #' @param n_synth (`numeric(1)`) \cr 
+    #' The number of samples drawn from the marginal distributions (default 10L).
+    #' @param n_iterations (`numeric(1)`) \cr 
+    #' The number of iteration. In each iteration a new terminal node is chosen 
+    #' from which the `n_synth` candidates are drawn.
+    #' @param feature_selector (`character(1)`)\cr
+    #' The method to choose features that are fixed for a counterfactual and which 
+    #' are part of the conditioning set when choosing tree paths. The default 
+    #' `random_importance` means that the probability of being in the conditioning set 
+    #' is proportional to how unimportant a feature is. 
+    #' The strategy `importance` means that all features are chosen except for 
+    #' (or less than) the `max_feats_to_change` features that are most important. 
+    #' The strategy `random` randomly chooses up to `max_feats_to_change` 
+    #' features that are not part of the conditioning set. All others are part.
+    #' @param importance_method (`character(1)`)\cr 
+    #' The local importance method to variables for the conditioning set. 
+    #' Ignored if `feature_selector = "random"`. 
+    #' Either "fastshap" based on the `fastshap` package (default) or "icesd" 
+    #' based on the standard deviation of the ICE curve is possible.
+    #' 
+    #' @export
+    initialize = function(predictor, max_feats_to_change = 1L, n_synth = 10L, n_iterations = 10L, feature_selector = "random_importance", importance_method = "fastshap") { 
       # TODO: add other hyperparameter
       super$initialize(predictor)
       checkmate::assert_integerish(max_feats_to_change, lower = 1L, upper = predictor$data$n.features)
@@ -14,6 +72,7 @@ CountARFactualClassif = R6::R6Class("CountARFactualClassif",
       private$feature_selector = feature_selector
       private$max_feats_to_change = max_feats_to_change
       private$n_synth = n_synth
+      private$n_iterations = n_iterations
       private$importance_method = importance_method
     }
   ),
@@ -21,6 +80,7 @@ CountARFactualClassif = R6::R6Class("CountARFactualClassif",
   private = list(
     max_feats_to_change = NULL,
     n_synth = NULL,
+    n_iterations = NULL,
     feature_selector = NULL,
     importance_method = NULL,
     run = function() {
@@ -55,9 +115,10 @@ CountARFactualClassif = R6::R6Class("CountARFactualClassif",
       
       x_interest = private$x_interest
       # TODO: which desired_prob to use when interval??
+      # --> Currently mean
       x_interest[, yhat := mean(private$desired_prob)] 
       synth = data.table()
-      for (i in 1:10) {
+      for (i in 1:private$n_iterations) {
         feats_not_to_change = sample(seq(
           private$predictor$data$n.features - private$max_feats_to_change, 
           private$predictor$data$n.features - 1L), size = 1L)
