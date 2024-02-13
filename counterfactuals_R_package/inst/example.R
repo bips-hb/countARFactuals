f7513e3b1477665d0a12a118c024ded98cb835d7
susanne-207 <dandl.susanne@gmail.com>
2024-02-13 18:03:26 +0100

    add neg_log as another measure besides train_dist

--- counterfactuals_R_package/inst/example.R
+++ counterfactuals_R_package/inst/example.R
@@ -1,5 +1,7 @@
 library("randomForest")
 
+
+
 data(iris, package = "datasets")
 x_interest = iris[150L, ]
 dat = data.table(iris[-150L, ])
@@ -18,17 +20,23 @@ dat[, Species := NULL]
 dat[, yhat := predictor$predict(dat)[,"virginica"]]
 
 frst = adversarial_rf(dat, always.split.variables = "yhat")
+fd = forde(frst, dat)
 
 # Find counterfactuals for x_interest
-load_all("counterfactuals_R_package/")
+load_all()
 
 moc_classif = MOCClassif$new(predictor, n_generations = 15L, 
   quiet = TRUE, conditional_mutator = "arf_multi", plausibility_measure = "lik", 
   arf = frst)
 
+set.seed(1234L)
+set.seed(1234567L)
 cfactuals = moc_classif$find_counterfactuals(
   x_interest = x_interest, desired_class = "virginica", desired_prob = c(0, 0.5)
 )
+
+cfactuals$evaluate_set(plausbility_measure = "lik", arf = frst)
+
 # Print the counterfactuals
 cfactuals$data
 # Plot evolution of hypervolume and mean and minimum objective values
@@ -45,7 +53,7 @@ cfactuals = arf_classif$find_counterfactuals(
 # Print the counterfactuals
 cfactuals$data
 # Plot evolution of hypervolume and mean and minimum objective values
-cfactuals$evaluate_set()
+cfactuals$evaluate_set(plausbility_measure = "lik", arf = frst)
 cfactuals$plot_parallel()
 
 
