a74b1386edc2381b64d20f6255d30e139ae92587
gcskoenig <gunnar.koenig@pm.me>
2024-02-14 17:58:21 +0100

    added drafts for 2d examples; draft for more general DGP class

---
+++ python/example_model.py
@@ -0,0 +1,213 @@
+
+import torch
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import pyro.distributions as dist
+
+def model_pawelczyk():
+    mix = dist.Categorical(torch.ones(3,))
+    comp = dist.Independent(dist.Normal(torch.tensor([[-10.0,5], [0,5], [10,0]]), torch.ones(3,2)), 1)
+    gmm = dist.MixtureSameFamily(mix, comp)
+    f = lambda sample: sample[:,1] > 6 
+    return gmm, f
+
+gmm, f = model_pawelczyk()
+data = gmm.sample((1000,)).numpy()
+y = f(data)
+plt.scatter(data[:,0], data[:,1], c=y)
+plt.colorbar()  # Add color scale/legend
+plt.show()
+gmm.log_prob(torch.tensor(data))
+
+class DGP:
+    def __init__(self):
+        pass
+    
+    def generate_data(self, n):
+        raise NotImplementedError
+    
+    def get_log_likelihood(self, x):
+        raise NotImplementedError
+    
+
+class TwoSines(DGP):
+    def __init__(self):
+        super().__init__()
+    
+    def generate_data(self, n):
+        mix = dist.Categorical(torch.ones(2,)).sample((n,))
+        x1 = dist.Normal(mix*1.0, 3.0).sample()
+        x2 = dist.Normal(torch.sin(x1) - 2.0 * mix + 1, 0.3).sample() 
+        data = torch.stack((x1, x2, mix)).numpy()
+        names = ['x1', 'x2', 'y']
+        return pd.DataFrame(data.T, columns=names)
+    
+    def get_log_likelihood(self, data):
+        # gets log likelihood ignoring y
+        log_probss = []
+        for y in [0, 1]:
+            x1 = torch.tensor(data['x1'])
+            x2 = torch.tensor(data['x2'])
+            y_ = torch.tensor(y)
+
+            d_x1 = dist.Normal(y_*1.0, 3.0)
+            d_x2 = dist.Normal(torch.sin(x1) - 2.0 * y_ + 1, 0.3)
+            log_probs = d_x1.log_prob(x1) + d_x2.log_prob(x2)
+            log_probss.append(log_probs)
+        log_probss = torch.stack(log_probss)
+        return torch.logsumexp(log_probss, 0).numpy()
+
+tm = TwoSines()
+data = tm.generate_data(1000)
+likelihood = tm.get_log_likelihood(data)
+
+plt.scatter(data['x1'], data['x2'], c=data['y'])
+plt.colorbar()  # Add color scale/legend
+plt.show()
+    
+
+class TwoMoons(DGP):
+    def __init__(self, noise=0.1):
+        super().__init__()
+        self.noise = 0.1
+
+    def generate_data(self, n):
+        y = dist.Categorical(torch.ones(2,)).sample((n,))
+
+        pos = dist.Uniform(0, torch.pi).sample((n,))
+        x1 = torch.cos(pos) * torch.pow(-1, y) + y
+        # outer_circ_x = np.cos(np.linspace(0, np.pi, n_samples_out))
+        # inner_circ_x = 1 - np.cos(np.linspace(0, np.pi, n_samples_in))
+        x2 = torch.sin(pos) * torch.pow(-1, y) + y * 0.5
+        # outer_circ_y = np.sin(np.linspace(0, np.pi, n_samples_out))
+        # inner_circ_y = 1 - np.sin(np.linspace(0, np.pi, n_samples_in)) - 0.5
+        noise = dist.Normal(0, self.noise).sample((n, 2))
+        x1 = x1 + noise[:, 0]
+        x2 = x2 + noise[:, 1]
+        data = [x1, x2, y]
+        names = ['x1', 'x2', 'y']
+        return pd.DataFrame(torch.stack(data).T.numpy(), columns=names)
+
+
+data = TwoMoons().generate_data(1000)
+
+plt.scatter(data['x1'], data['x2'], c=data['y'])
+plt.show()
+
+
+class Cassini(DGP):
+    def __init__(self):
+        super().__init__()
+
+    def generate_data(self, n):
+        y1 = dist.Categorical(torch.tensor([1,2])).sample((n,))
+        y2 = dist.Categorical(torch.ones(2,)).sample((n,))
+        y = y1 + y1 * y2
+
+        x1_center = dist.Normal(0.0, 0.2)
+        x1_outer = dist.Normal(0.0, 0.5)
+
+        x1_mix = dist.MaskedMixture(y1.to(torch.bool), x1_center, x1_outer).sample()
+
+        x2_center = dist.Normal(0.0*x1_mix, 0.2)
+        x2_outer = dist.Normal(torch.cos(x1_mix)*torch.pow(-1, y2), 0.2)
+
+        x2_mix = dist.MaskedMixture(y1.to(torch.bool), x2_center, x2_outer).sample()
+
+        data = [x1_mix.numpy(), x2_mix.numpy(), y.numpy()]
+        names = ['x1', 'x2', 'y']
+        return pd.DataFrame(np.stack(data).T, columns=names)
+
+
+    def get_log_likelihood(self, data):
+        x1_obs = torch.tensor(data['x1'])
+        x2_obs = torch.tensor(data['x2'])
+        
+        combs = [(0,0), (1, 0), (1, 1)]
+        logs_probss = []
+        for (y1, y2) in combs:
+            y1, y2 = torch.tensor(y1).repeat(x1_obs.shape[0]), torch.tensor(y2).repeat(x1_obs.shape[0])
+            x1_center = dist.Normal(0.0, 0.2)
+            x1_outer = dist.Normal(0.0, 0.5)
+
+            x1_lls = dist.MaskedMixture(y1.to(torch.bool), x1_center, x1_outer).log_prob(x1_obs)
+
+            x2_center = dist.Normal(0.0*x1_obs, 0.2)
+            x2_outer = dist.Normal(torch.cos(x1_obs)*torch.pow(-1, y2), 0.2)
+
+            x2_lls = dist.MaskedMixture(y1.to(torch.bool), x2_center, x2_outer).log_prob(x2_obs)
+
+            log_probs = x1_lls + x2_lls
+            logs_probss.append(log_probs)
+        logs_probss = torch.stack(logs_probss)
+        return torch.logsumexp(logs_probss, 0).numpy()
+
+data = Cassini().generate_data(1000)
+plt.scatter(data['x1'], data['x2'], c=data['y'])
+plt.show()
+
+steps = np.linspace(-1, 1, 100)
+x1, x2 = np.meshgrid(steps, steps)
+grid = pd.DataFrame({'x1': x1.ravel(), 'x2': x2.ravel()})
+
+likelihood = Cassini().get_log_likelihood(grid)
+plt.scatter(x1, x2, c=likelihood)
+plt.colorbar()  # Add color scale/legend
+plt.show()
+
+
+class Shapes(DGP):
+    def __init__(self):
+        super().__init__()
+
+    def generate_data(self, n):
+        yh = dist.Categorical(torch.ones(2)).sample((n,))
+        yv = dist.Categorical(torch.ones(2)).sample((n,))
+        y = yh + 2.0*yv
+
+        # blob top left
+        blob_x1 = dist.Normal(torch.tensor([-1.0]), torch.tensor([0.2]))
+
+        # rectangle bottom left
+        rect_x1 = dist.Uniform(torch.tensor([-1.5]), torch.tensor([-0.5]))
+
+        # triangle top right
+        triangle_x1 = dist.Uniform(torch.tensor(0.0), torch.tensor(2.0))
+
+        # sine wave bottom right
+        sine_x1 = dist.Uniform(torch.tensor(0.0), torch.tensor(2.0))
+
+        # separate mixing distributions for x1 and x2
+        # three mixtures for x1, three for x2 (one for yh and two for yv each)
+
+        dist_x1 = dist.MaskedMixture(yh.to(torch.bool),
+                                     dist.MaskedMixture(yv.to(torch.bool), rect_x1, blob_x1),
+                                     dist.MaskedMixture(yv.to(torch.bool), sine_x1, triangle_x1))
+        
+        x1 = dist_x1.sample()
+        
+        rect_x2 = dist.Uniform(torch.tensor([-2.0]).repeat(n), torch.tensor([0]).repeat(n))
+        blob_x2 = dist.Normal(torch.tensor([1.0]).repeat(n), torch.tensor([0.2]).repeat(n))
+        spread = torch.abs((0.5 - torch.abs(1 - x1) / 2.0)) * yv * yh + torch.finfo(torch.float32).eps
+        triangle_x2 = dist.Uniform(torch.tensor(1.0).repeat(n), torch.tensor(1.0 + spread))
+        sine_x2 = dist.Normal(10*torch.sin(x1*torch.pi), torch.tensor(0.5))
+
+        dist_x2 = dist.MaskedMixture(yh.to(torch.bool),
+                                     dist.MaskedMixture(yv.to(torch.bool), rect_x2, blob_x2),
+                                     dist.MaskedMixture(yv.to(torch.bool), sine_x2, triangle_x2))
+
+        x1, x2 = dist_x1.sample(), dist_x2.sample()
+
+        data = [x1.numpy(), x2.numpy(), y.numpy()]
+        names = ['x1', 'x2', 'y']
+        return pd.DataFrame(np.stack(data).T, columns=names)
+
+data = Shapes().generate_data(10000)
+
+data_sine = data.loc[data['y'] == 3, :]
+
+plt.scatter(data_sine['x1'], data_sine['x2'])
+plt.show()
--- python/simulation.py
+++ python/simulation.py
@@ -1,33 +1,227 @@
-
+import networkx as nx
+import random
+import torch
+import pyro.distributions as dist
+import pandas as pd
 
 
 class DGP:
-    def __init__(self, d, graph=None, model=None):
-        self.d = d
-        # set graph, generate one if not available
-        if graph is None:
-            self.graph = self._generate_graph()
-        else:
-            self.graph = graph
+    """ A class to represent a broad class of Bayesian Networks.
+    The user specifies a directed acyclic graph (DAG) and a set of conditional distributions.
+    The class can then be used to sample from the model and to calculate the log likelihood of the data.
+    For efficient computation, the computation is parallelized over the batch dimension.
+    The batch dimension must be speficied when creating the class but can be changed later.
+    """
+    def __init__(self, graph, dist_fncs, batch_size):
+        """
+        Args:
+            graph (nx.DiGraph): A directed acyclic graph.
+            dist_fncs (dict): A dictionary of conditional distributions, where the keys are the names of the nodes.
+            batch_size (int): The batch size.
+        """
+        self.graph = graph  
+        self.nodes = list(nx.topological_sort(self.graph))
+        self.fncs = dist_fncs
+        self.values = {}
+        self.dists = {}
+        self.batch_size = batch_size
+
+    @staticmethod
+    def generate_graph(d, p=0.5, seed=None):
+        if seed is None:
+            seed = random.randint(0, 1000)
+        graph = nx.gnp_random_graph(d, p, seed, directed=True)
+        graph.remove_edges_from([(u, v) for (u, v) in graph.edges() if u > v])
+        assert nx.is_directed_acyclic_graph(graph)
+        # get max degree node
+        degrees = dict(nx.degree(graph))
+        y = max(degrees, key=degrees.get)
+        # relabel nodes
+        nodes = list(nx.topological_sort(graph))
+        mapping = {}
+        ii = 1
+        for node in nodes:
+            if node != y:
+                mapping[node] = 'x{}'.format(ii)
+                ii += 1
+            else:
+                mapping[node] = 'y'
+        graph = nx.relabel_nodes(graph, mapping)
+        return graph
+    
+    @staticmethod
+    def generate_model(graph=None):
+        raise NotImplementedError
         
-        # set model, generate one if not available
-        if model is None:
-            self.model = self._generate_model()
-        else:
-            self.model = model
+    def set_batch_size(self, batch_size):
+        """Allows changing the batch size.
+        
+        Args:
+            batch_size (int): The new batch size.
+        """
+        # delete all values if batch size changes
+        if self.batch_size != batch_size:
+            print('All values reset with batch_size change.')
+            for key in self.values.keys():
+                self.values[key] = None
+        self.batch_size = batch_size
 
-    def _generate_graph(self):
+    def set_values(self, values):
+        """Sets the values of the nodes. When setting the values the conditional distribution
+        objects are updated as well to reflect the changes in parent values.
+
+        Args:
+            values: A pandas DataFrame or a torch.Tensor with the values of the nodes.
+                (The last dimension must be the same as the number of nodes,
+                and the first dimension must be the batch size.)
+        """
+        # sets values and updates dists
+        assert values.shape[-1] == len(self.nodes)
+        self.set_batch_size(values.shape[0])
+        for node in self.nodes:
+            # store values
+            if isinstance(values, pd.DataFrame):
+                self.values[node] = torch.tensor(values[node])
+            elif isinstance(values, torch.Tensor):
+                self.values[node] = values[..., self.nodes.index(node)] # set values
+            else:
+                raise NotImplementedError
+            
+            # update dists
+            self.dists[node] = self.get_dist_node(node) # adapt dists according to parent values
+
+    def get_values(self, as_df=True):
+        """Allows retrieving the values as a pd.DataFrame.
+        """
+        if as_df:
+            vals = []
+            for node in self.nodes:
+                vals.append(self.values[node])
+            vals = torch.stack(vals, -1).numpy()
+            return pd.DataFrame(vals, columns=self.nodes)
         raise NotImplementedError
+        
+    def get_parents(self, node):
+        """Returns the parents of a node sorted topologically."""
+        assert node in self.graph.nodes
+        parents = list(self.graph.predecessors(node))
+        parents = sorted(parents, key=self.nodes.index)
+        return parents
     
-    def _generate_model(self):
-        raise NotImplementedError
+    def is_root(self, node):
+        """Returns True if the node is a root node."""
+        return len(self.get_parents(node)) == 0
     
-    def generate_data(self, n):
-        raise NotImplementedError
+    def get_parents_values(self, node):
+        """Returns the values of the parents of a node."""
+        if self.is_root(node):
+            return torch.tensor([])
+        parents = self.get_parents(node)
+        valss = []
+        for parent in parents:
+            assert not self.values[parent] is None
+            valss.append(self.values[parent])
+        valss = torch.stack(valss, -1)
+        return valss    
     
-    def get_likelyhood(self, n):
-        raise NotImplementedError
-    
+    def get_dist_node(self, node):
+        """Returns the conditional distribution of a node given its parents.
+        The parents values are stored in the DGP object and can be updated by 
+        calling the set_values method.
+        """
+        parent_values = self.get_parents_values(node)
+        dist = self.fncs[node](parent_values)
+        if self.is_root(node):
+            dist = dist.expand([self.batch_size])
+        assert dist.batch_shape == torch.Size([self.batch_size])
+        assert dist.event_shape == torch.Size([])
+        self.dists[node] = dist
+        return dist
+
+    def sample_node(self, node):
+        """Samples a node given the values of its parents.
+        The parent values are stored in the DGP object and can be updated using the
+        set_values method.
+        """
+        dist = self.get_dist_node(node)
+        sample = dist.sample()
+        assert sample.shape == torch.Size([self.batch_size])
+        self.values[node] = sample
+        return sample
+
+    def sample(self):
+        """Samples from the model according to the batch size."""
+        samples = []
+        for node in nx.topological_sort(self.graph):
+            sample = self.sample_node(node)
+            samples.append(sample)
+        samples = torch.stack(samples, -1)
+        assert samples.shape == torch.Size([self.batch_size, len(self.graph.nodes)])
+        return samples
+
+    def log_prob_node(self, node):
+        """Gets the log prob of a node given the values of its parents.
+        The values of node and parents are stored in the DGP 
+        and can be updated using the set_values method."""
+        log_likelihood = self.dists[node].log_prob(self.values[node])
+        return log_likelihood
+
+    def log_prob(self, data, return_df=False):
+        """Calculates the log likelihood of the data."""
+        self.set_values(data)
+        log_probs = []
+        for node in self.nodes:
+            log_probs.append(self.log_prob_node(node))
+        log_prob = torch.stack(log_probs, -1).sum(-1)
+        if return_df:
+            df = self.get_values(as_df=True)
+            df['log_prob'] = log_prob
+            return df
+        return log_prob
 
 
-    
+if __name__ == "__main__":
+    # generate graph 
+        
+    graph = nx.DiGraph()
+    graph.add_node('x1')
+    graph.add_node('x2')
+    graph.add_node('x3')
+    graph.add_node('x4')
+    graph.add_node('y')
+
+    graph.add_edge('x1', 'x3')
+    graph.add_edge('x2', 'x3')
+    graph.add_edge('x3', 'x4')
+    graph.add_edge('x1', 'x4')
+    graph.add_edge('x2', 'x4')
+    graph.add_edge('x4', 'y')
+    graph.add_edge('x2', 'y')
+
+    # specify the conditional distributions
+
+    dist_fncs = {}
+    dist_fncs['x1'] = lambda pv: dist.Binomial(probs=torch.tensor([0.7]))
+    dist_fncs['x2'] = lambda pv: dist.Normal(torch.tensor([0.0]), torch.tensor([1.0]))
+    dist_fncs['x3'] = lambda pv: dist.Binomial(probs=torch.sigmoid(pv.sum(-1)))
+    dist_fncs['x4'] = lambda pv: dist.Normal(pv.sum(-1), 1.0)
+    dist_fncs['y'] = lambda pv: dist.Binomial(probs=torch.sigmoid(pv.sum(-1)))
+
+    dgp = DGP(graph, dist_fncs, 1000)
+    values = dgp.sample()
+
+    dgp.log_prob_node('x1')
+    dgp.log_prob_node('x3')
+    df_likelihood = dgp.log_prob(values, return_df=True)
+
+    print(df_likelihood.head())
+
+    # import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    # sns.pairplot(df_likelihood, vars=['x1', 'x2', 'x3', 'x4'], hue='y')
+    # plt.show()
+
+    graph = DGP.generate_graph(20, 0.8)
+    nx.draw(graph, with_labels=True)
+    plt.show()
