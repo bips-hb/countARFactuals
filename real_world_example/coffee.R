abf624bc3f553d733dbbb2fca16182dde494b616
susanne-207 <dandl.susanne@gmail.com>
2024-02-15 19:15:02 +0100

    init coffee preprocessing

---
+++ real_world_example/coffee.R
@@ -0,0 +1,57 @@
+########## COFFEE DATASET ######
+
+## SOURCES: 
+# https://github.com/jldbc/coffee-quality-database/tree/master/data
+# https://www.kaggle.com/code/reminho/coffee-quality-score-prediction-using-tree-models
+
+
+# required packages
+library(data.table)
+library(dplyr)
+library(forcats)
+
+# readin
+coffee = data.table(read.csv("real_world_example/arabica_data_cleaned.csv", sep = ",", header = TRUE, row.names = "X"))
+dim(coffee)
+
+## Preproocessing
+# blanks to NAs
+indx2 <- which(sapply(coffee, is.character)) 
+for (j in indx2) set(coffee, i = grep("^$|^ $", coffee[[j]]), j = j, value = NA_character_)
+
+# column names to lower
+setnames(coffee,tolower(names(coffee)))
+
+# keep everything but quality measures and (presumably) non-informative columns
+coffee <- coffee %>%
+  # select everything except quality measures
+  select(country.of.origin, harvest.year, variety:moisture, color, altitude_mean_meters)
+
+# summary
+dim(coffee)
+
+# omit all NAs in dataset
+coffee = na.omit(coffee)
+dim(coffee)
+
+# replace weird year format
+coffee <- coffee %>%
+  mutate(harvest.year = str_replace_all(harvest.year, regex('^\\d+\\s?/\\s?(\\d+)'), regex('\\1'))) %>% 
+  mutate(harvest.year = str_replace_all(harvest.year, regex('^\\d+\\s?-\\s?(\\d+)'), regex('\\1'))) 
+
+# table(coffee$harvest.year)
+# table(coffee$processing.method)
+# table(coffee$color)
+# table(coffee$variety)
+
+# bin categories of variety
+coffee = coffee %>%
+  mutate(variety = fct_lump_n(variety, 5))
+
+# some "outliers" in altitute_mean_meters?
+plot(density(coffee$altitude_mean_meters))
+sort(coffee$altitude_mean_meters)
+coffee = coffee[altitude_mean_meters < 8000,]
+dim(coffee)
+
+write.csv(coffee, "cleaned_data_set.csv")
