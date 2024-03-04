7f48dcd55aada4b53b2474934f73d33d2b7f8043
susanne-207 <dandl.susanne@gmail.com>
2024-03-04 11:16:10 +0100

    evaluate results

---
+++ R_experiments/aggregate_results.R
@@ -0,0 +1,62 @@
+# --- VISUALIZE RESULTS ----
+library(data.table)
+library(batchtools)
+
+
+eval_columns = c("dist_x_interest", "no_changed", "neg_lik", "dist_train", 
+  "dist_x_interest_nondom", "no_changed_nondom", "neg_lik_nondom", 
+  "dist_train_nondom", "log_probs", "runtime")
+save_columns = c(eval_columns, "id", "job.id", "n_synth", "problem", "cf_method", "dataset", "nondom")
+csv_dir = "cfs/29_02"
+
+# Get results -------------------------------------------------------------
+
+res = NULL
+files = list.files(csv_dir)
+files = files[grepl(files, pattern = "log_probs.csv")]
+for (file in files) {
+  print(file)
+  tmp = fread(file = file.path(csv_dir, file), header = TRUE)[, ..save_columns]
+  res = rbind(res, tmp)
+}
+
+# Preprocessing ------------------------------------------------------------
+res = res[, log_probs := exp(log_probs)]
+res[, method := ifelse(cf_method == "ARF", paste(cf_method, n_synth), cf_method)]
+res = res[, implausibility := 1-log_probs]
+res = res[, max_no_changed := max(no_changed), by = .(method, dataset, id)]
+res = res[, rel_no_changed := no_changed/max(no_changed), by = .(method, dataset, id)]
+# saveRDS(res, "R_experiments/res.Rds")
+
+# Averages per objective ---------------------------------------------------
+res_mean = res[, lapply(.SD, mean), .SDcols = eval_columns, by = .(method, dataset, id)]
+res_log_probs_nondom = res[nondom == TRUE, lapply(.SD, mean), .SDcols = "log_probs", by = .(method, dataset, id)]
+setnames(res_log_probs_nondom, "log_probs", "log_probs_nondom")
+res_mean = merge(res_mean, res_log_probs_nondom, by = c("method", "dataset", "id"))
+
+# Correlation estimated vs. true implausibility ------------------------------
+dt_lik = res[, .(cor_lik = cor(log_probs, -neg_lik, method = "spearman")), by = .(method, dataset, id)]
+dt_gow = res[, .(cor_gow = cor(log_probs, -dist_train, method = "spearman")), by = .(method, dataset, id)]
+corr = merge(dt_lik, dt_gow, by = c("method", "dataset", "id"))
+res_mean = merge(res_mean, corr, by = c("method", "dataset", "id"))
+
+# HV on o_prox, o_sparse and true implausibility ------------------------------
+hv_measures = c("dist_x_interest", "rel_no_changed", "implausibility")
+nadir = res[, lapply(.SD, max), .SDcols = hv_measures]
+get_hv = function(x) {
+  miesmuschel::domhv(fitnesses = -as.matrix(rbind(x)), nadir = -as.numeric(nadir))
+}
+hvs = res[, hv := get_hv(.SD), .SDcols = hv_measures, by = .(method, dataset, id)]
+hvs_nondom = res[nondom == TRUE, hv_nondom := get_hv(.SD), 
+  .SDcols = hv_measures, by = .(method, dataset, id)]
+hvs_mean = res[, lapply(.SD, mean, na.rm = TRUE), .SDcols = c("hv", "hv_nondom"), by = .(method, dataset, id)]
+res_mean = merge(res_mean, hvs_mean, by = c("method", "dataset", "id"))
+
+# Number of CFEs -------------------------------------------------------------
+res[, number := .N, by = .(method, dataset, id)]
+res[nondom == TRUE, number_nondom := .N, by = .(method, dataset, id)]
+no_mean = res[, lapply(.SD, mean, na.rm = TRUE), .SDcols = c("number", "number_nondom"), 
+  by = .(method, dataset, id)]
+res_mean = merge(res_mean, no_mean, by = c("method", "dataset", "id"))
+
+# saveRDS(res_mean, "R_experiments/res_mean.Rds")
--- R_experiments/evaluate_results.R
+++ R_experiments/evaluate_results.R
@@ -1,58 +1,108 @@
-# --- VISUALIZE RESULTS ----
-library(data.table)
-library(batchtools)
 library(ggplot2)
 library(reshape2)
-library(gridExtra)
+library(scales)
+library(patchwork)
+res_folder = file.path("R_experiments/results", Sys.Date())
+dir.create(res_folder)
 
+res_mean = readRDS("R_experiments/res_mean.Rds")
 
-eval_columns = c("dist_x_interest", "no_changed", "neg_lik", "dist_train", 
-  "dist_x_interest_nondom", "no_changed_nondom", "neg_lik_nondom", 
-  "dist_train_nondom", "log_probs", "runtime")
-save_columns = c(eval_columns, "id", "job.id", "n_synth", "problem", "cf_method", "dataset", "nondom")
-csv_dir = "cfs/29_02"
+# Correlation analysis ------------------------------------------------------------
+# FIXME: some correlations NA because all values constant (0 or 1)
+# - 0 plausibility in bn_50_v2 (ARF, MOC), additionally two_sines, bn_5_v2 by NICE
+# Removed those from evaluation --> all of Whatif, some of the others
+corr_data = res_mean[!is.na(cor_lik) & !is.na(cor_gow),]
+wilcox.test(corr_data$cor_lik, corr_data$cor_gow, alternative = "greater", paired = TRUE)
+table(is.na(res_mean$cor_gow), res_mean$method)
 
-# Get results -------------------------------------------------------------
+# Correlation 
+boxplot(cor_lik ~ method, corr_data)
+boxplot(cor_gow ~ method, corr_data)
+# FIXME: weird things with NICE :/
 
-res = NULL
-files = list.files(csv_dir)
-files = files[grepl(files, pattern = "log_probs.csv")]
-for (file in files) {
-  print(file)
-  tmp = fread(file = file.path(csv_dir, file), header = TRUE)[, ..save_columns]
-  res = rbind(res, tmp)
+# Plot results -------------------------------------------------------------
+res_mean[, "log(runtime)" := log(runtime)]
+res_mean[, "log(probs)" := log(log_probs)]
+res_mean[, "log(probs_nondom)" := log(log_probs_nondom)]
+res_mean[, "log(hv)" := log(hv)]
+res_mean[, "log(hv_nondom)" := log(hv_nondom)]
+res_mean[, "log(number)" := log(number)]
+res_mean[, "log(number_nondom)" := log(number_nondom)]
+res_mean$dataset = factor(res_mean$dataset, levels = c("cassini", "pawelczyk", 
+  "two_sines", "bn_5_v2", "bn_10_v2", "bn_20", "bn_50_v2"))
+res_mean$method = factor(res_mean$method, levels = c("WhatIf", "NICE", "MOC", 
+  "MOCCTREE", "MOCARF", "ARF 20", "ARF 200"))
+
+# rename objectives
+setnames(res_mean, old = c("dist_x_interest", "no_changed", "dist_train", 
+  "dist_x_interest_nondom", "no_changed_nondom", "dist_train_nondom"), 
+  new = c("o_prox", "o_sparse", "o_plaus", "o_prox_nondom", "o_sparse_nondom", 
+    "o_plaus_nondom"))
+
+plotdata = data.table(melt(res_mean, id.vars=c("method", "dataset", "id")))
+plotdata_all = plotdata[!grepl("nondom", plotdata$variable),]
+plotdata_nondom = plotdata[grepl("nondom", plotdata$variable),]
+
+plot_results = function(data, evaluation_measures = NULL, remove_strip_x = FALSE) {
+  if (!is.null(evaluation_measures)) {
+    data = data[variable %in% evaluation_measures, ]
+  }
+  
+  pl = ggplot(data, aes(x = method, y = value, fill = method)) + 
+    geom_boxplot() + 
+    coord_flip() +
+    facet_grid(variable ~ dataset, scales = "free_x") + 
+    theme_bw() +
+    scale_color_brewer(palette="BrBG") + 
+    scale_y_continuous(breaks = pretty_breaks(n = 3)) + 
+    theme(legend.position="none", 
+      axis.title.x = element_blank(), 
+      axis.title.y = element_blank(), 
+      panel.spacing.x = unit(4, "mm"),
+      plot.margin=unit(c(0, 0, 0, 0), "cm")) 
+  if (remove_strip_x) {
+    pl = pl + theme(strip.text.x = element_blank())
+  }
+  pl
+  pl
+  
 }
 
-# Preprocessing ------------------------------------------------------------
-res = res[, log_probs := exp(log_probs)]
-res[, method := ifelse(cf_method == "ARF", paste(cf_method, n_synth), cf_method)]
-res = res[, plausibility := 1-log_probs]
-res = res[, max_no_changed := max(no_changed), by = .(method, dataset, id)]
-res = res[, rel_no_changed := no_changed/max(no_changed), by = .(method, dataset, id)]
-# saveRDS(res, "R_experiments/res.Rds")
+# objectives
+p_prox = plot_results(plotdata_all, "o_prox")
+p_sparse = plot_results(plotdata_all, "o_sparse", remove_strip_x = TRUE)
+p_plaus = plot_results(plotdata_all, "o_plaus", remove_strip_x = TRUE)
+p_neglik = plot_results(plotdata_all, "neg_lik", remove_strip_x = TRUE)
+obj_plot = p_prox/p_sparse/p_plaus/p_neglik
 
-# Averages per objective ---------------------------------------------------
-res_mean = res[, lapply(.SD, mean), .SDcols = eval_columns, by = .(method, dataset, id)]
-res_log_probs_nondom = res[nondom == TRUE, lapply(.SD, mean), .SDcols = "log_probs", by = .(method, dataset, id)]
-setnames(res_log_probs_nondom, "log_probs", "log_probs_nondom")
-res_mean = merge(res_mean, res_log_probs_nondom, by = c("method", "dataset", "id"))
+ggsave(filename = file.path(res_folder,"results_objectives.png"), plot = obj_plot, 
+  dpi = 200, width = 11, height = 6)
 
-# Correlation estimated vs. true plausibility ------------------------------
-dt_lik = res[, .(cor_lik = cor(log_probs, -neg_lik, method = "spearman")), by = .(method, dataset, id)]
-dt_gow = res[, .(cor_gow = cor(log_probs, -dist_train, method = "spearman")), by = .(method, dataset, id)]
-corr = merge(dt_lik, dt_gow, by = c("method", "dataset", "id"))
-res_mean = merge(res_mean, corr, by = c("method", "dataset", "id"))
+# nondom 
+p_prox_nondom = plot_results(plotdata_nondom, "o_prox_nondom")
+p_sparse_nondom = plot_results(plotdata_nondom, "o_sparse_nondom", remove_strip_x = TRUE)
+p_plaus_nondom = plot_results(plotdata_nondom, "o_plaus_nondom", remove_strip_x = TRUE)
+p_neglik_nondom = plot_results(plotdata_nondom, "neg_lik_nondom", remove_strip_x = TRUE)
 
-# HV on o_prox, o_sparse and true plausibility ------------------------------
-hv_measures = c("dist_x_interest", "rel_no_changed", "plausibility")
-nadir = res[, lapply(.SD, max), .SDcols = hv_measures]
-get_hv = function(x) {
-  miesmuschel::domhv(fitnesses = -as.matrix(rbind(x)), nadir = -as.numeric(nadir))
-}
-hvs = res[, hv := get_hv(.SD), .SDcols = hv_measures, by = .(method, dataset, id)]
-hvs_nondom = res[nondom == TRUE, hv_nondom := get_hv(.SD), 
-  .SDcols = hv_measures, by = .(method, dataset, id)]
-hvs_mean = res[, lapply(.SD, mean, na.rm = TRUE), .SDcols = c("hv", "hv_nondom"), by = .(method, dataset, id)]
-res_mean = merge(res_mean, hvs_mean, by = c("method", "dataset", "id"))
+obj_plot_nondom = p_prox_nondom/ p_sparse_nondom/ p_plaus_nondom/ p_neglik_nondom
+ggsave(filename = file.path(res_folder,"results_objectives_nondom.png"), plot = obj_plot_nondom, 
+  dpi = 200, width = 11, height = 6)
 
-saveRDS(res_mean, "R_experiments/res_mean.Rds")
+# selling points 
+p_probs = plot_results(plotdata_all, "log(probs)")
+p_probs_nondom = plot_results(plotdata_nondom, "log(probs_nondom)", remove_strip_x = TRUE)
+p_hv = plot_results(plotdata, "log(hv)", remove_strip_x = TRUE)
+p_hv_nondom = plot_results(plotdata_nondom, "log(hv_nondom)", remove_strip_x = TRUE)
+
+sell_obj = p_probs/p_probs_nondom/p_hv/p_hv_nondom
+ggsave(filename = file.path(res_folder,"logprobs_hv.png"), plot = sell_obj, 
+  dpi = 200, width = 10, height = 6)
+
+p_runtime = plot_results(plotdata_all, "log(runtime)")
+p_no = plot_results(plotdata_all, "log(number)", remove_strip_x = TRUE)
+p_no_nondom = plot_results(plotdata_nondom, "log(number_nondom)", remove_strip_x = TRUE)
+
+sell_time_no = p_runtime/p_no/p_no_nondom
+ggsave(filename = file.path(res_folder,"runtime_no.png"), plot = sell_time_no, 
+  dpi = 200, width = 10, height = 5)
+
