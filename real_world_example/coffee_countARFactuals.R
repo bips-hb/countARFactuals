d311c87f45e8b0072b1fdb873a5a68031b9e0f59
krisbl <kristin.blesch@web.de>
2024-03-07 14:16:54 +0100

    update coffee example

--- real_world_example/coffee_countARFactuals.R
+++ real_world_example/coffee_countARFactuals.R
@@ -1,14 +1,20 @@
 #library(counterfactuals)
 devtools::load_all("counterfactuals_R_package/")
 library(iml)
-library(arf) # requires CRAN version 0.2.0 
+library(arf) # using version 0.2.2 PR#20 
 library(ggplot2)
 
-set.seed(2024)
+set.seed(1)
 
 # read data
 coffee = read.csv("real_world_example/coffee_data_cleaned.csv")
 coffee$quality = as.factor(coffee$quality)
+coffee$processing.method  = as.factor(coffee$processing.method)
+coffee$variety  = as.factor(coffee$variety)
+coffee$color  = as.factor(coffee$color)
+
+coffee$cupper.points = NULL
+coffee$clean.cup = NULL
 
 # define actionable features
 actionable_features = c("country.of.origin",
@@ -16,44 +22,43 @@ actionable_features = c("country.of.origin",
                         "variety",
                         "processing.method",
                         "moisture",
-                        "color", 
-                        "acidity",
-                        "sweetness",
+                   #     "color", 
+                   #     "acidity",
+                    #    "sweetness",
                         "altitude_mean_meters",
                         "quality")
 
 # sample instance of interest
-x_int = coffee[sample(which(coffee$quality == "bad"), 1),]
-     
+#x_int = coffee[sample(which(coffee$quality == "bad"), 1),]
+x_int = coffee[577,]
+
 # Fit model, exclude x_int
 rf = randomForest::randomForest(quality ~ ., coffee[-as.numeric(rownames(x_int)),], probability = TRUE)
 
 # generate countARFactuals
 predictor = Predictor$new(rf, type = "prob" )
-countARFactual_classif = CountARFactualClassif$new(predictor, 
-                                                  # fixed_features =  names(coffee)[!names(coffee) %in% actionable_features], 
-                                                   max_feats_to_change = 3) # change max 3 features , max_feats_to_change = 3
+
 # Hyperparameters
 
+# Hyperparameters in R experiments
 # weight_coverage = c(1, 5, 20)
 # weight_proximity = c(1, 5, 20)
 # node_selector = c("coverage", "coverage_proximity")
 # n_synth = c(20L, 200L)
 # max_feats_to_change = 3
 
-weight_coverage = c(5)
-weight_proximity = c(20)
+weight_coverage = c(1)
+weight_proximity = c(5)
 node_selector = c("coverage_proximity")
-n_synth = c(200L)
-max_feats_to_change = 3
+n_synth = c(20L)
+max_feats_to_change = 10
 
 cac = CountARFactualClassif$new(predictor = predictor, max_feats_to_change = max_feats_to_change,
-                                weight_node_selector = c(weight_coverage, weight_proximity), 
+                                weight_node_selector = c(weight_coverage, weight_proximity),
+                                fixed_features =  names(coffee)[!names(coffee) %in% actionable_features],
                                 n_synth = n_synth, node_selector = node_selector)
 
 my_countARFactuals = cac$find_counterfactuals(x_interest = x_int, desired_class = "good", desired_prob = c(0.5,1)) 
-
-#my_countARFactuals = countARFactual_classif$find_counterfactuals(x_interest = x_int, desired_class = "good", desired_prob = c(0.5,1)) 
 
 # have a look
 my_countARFactuals$data
@@ -70,11 +75,7 @@ idnondom = miesmuschel::rank_nondominated(-as.matrix(res))$fronts == 1
 res_nondom = my_countARFactuals$data[idnondom, ]
 
 
-
-# # visualize results:
-# # plot frequency
-# my_countARFactuals$plot_freq_of_feature_changes(subset_zero = TRUE,
-#       feature_names = names(my_countARFactuals$get_freq_of_feature_changes(subset_zero = TRUE)[1:5]))
+# visualize results:
 
 # parallel plot of top 5 features
 my_countARFactuals$plot_parallel(feature_names = names(
@@ -83,11 +84,16 @@ my_countARFactuals$plot_parallel(feature_names = names(
 # ggsave("plt_parallel.png", plot = last_plot(), device = "png",path = "./real_world_example")
 
 # plot surface
-my_countARFactuals$plot_surface(feature_names = c('acidity', 'processing.method'))
+my_countARFactuals$plot_surface(feature_names = c('variety', 'processing.method'))
 # ggsave("plt_surface.png", plot = last_plot(), device = "png",path = "./real_world_example")
 
 
+res_nondom$quality = predict(rf, res_nondom) # why is this not always "good" quality?! this data is after $subset_to_valid()
+x_int = as.data.frame(x_int)
+row.names(x_int) = "x_interest"
+# write.csv(as.data.frame(rbind(x_int, res_nondom)), "./real_world_example/coffee_countARFactuals.csv")
+
+# Have a look
 res_nondom
 x_int
-
 
