a9e79672e3ecfb8e438567aaeacb4810f694f44a
Marvin Wright <github@wrig.de>
2024-03-13 23:33:05 +0100

    log plotting

--- R_experiments/evaluate_results.R
+++ R_experiments/evaluate_results.R
@@ -33,7 +33,16 @@ res[, "proximity" := 1 - dist_x_interest]
 res_agg = readRDS("R_experiments/res_agg.Rds")
 res_agg[, "runtime*" := log(runtime)]
 res_agg[, "number*" := log(number)]
+res_agg[, "hypervolume" := hv_normalized]
 res_agg[, "hv*" := log(hv_normalized)]
+
+# Remove bn_50 due to weird hv results
+res_agg <- res_agg[dataset != "bn_50_v2", ]
+res <- res[dataset != "bn_50_v2", ]
+
+# Scale hypervolume for plotting
+scale01 <- function(x){(x-min(x))/(max(x)-min(x))}
+res_agg[, hypervolume := scale01(hypervolume), by = dataset]
 
 res$dataset = factor(res$dataset, levels = c("cassini", "pawelczyk", 
   "two_sines", "bn_5_v2", "bn_10_v2", "bn_20", "bn_50_v2"), 
@@ -49,7 +58,8 @@ res$method = factor(res$method, levels = c("NICE", "MOC",
   "MOCCTREE", "MOCARF", "ARF"))
 
 ## remove one outlier in NICE HV 
-res_subset = res_agg[-which(res_agg$hv_normalized > 40 & res_agg$dataset == "bn_50"), ]
+#res_subset = res_agg[-which(res_agg$hv_normalized > .2 & res_agg$dataset == "bn_50"), ]
+res_subset = res_agg
 
 # ####  calculate ranks per objective ------
 # res_mean = res_mean[!method %in% c("ARF 200"), 
@@ -92,7 +102,8 @@ res_subset = res_agg[-which(res_agg$hv_normalized > 40 & res_agg$dataset == "bn_
 # 
 # boxplot(plausibility ~ method, data = res)
 
-plot_results = function(data, evaluation_measure = NULL, remove_strip_x = FALSE) {
+plot_results = function(data, evaluation_measure = NULL, remove_strip_x = FALSE, 
+                        log = FALSE) {
   if (!is.null(evaluation_measure)) {
     colnams = c("dataset", "method", "id", evaluation_measure)
     data = data[, ..colnams]
@@ -104,11 +115,10 @@ plot_results = function(data, evaluation_measure = NULL, remove_strip_x = FALSE)
   pl = ggplot(data, aes(x = method, y = evalmeasure, fill = method)) + 
     geom_boxplot() + 
     coord_flip() +
-    facet_grid(measure ~ dataset, scales = "free_x") + 
+    facet_grid(measure ~ dataset, scales = "free") + 
     theme_bw() + 
    #  scale_color_brewer(palette="BrBG") + 
     scale_fill_manual(values = colours) + 
-    scale_y_continuous(breaks = pretty_breaks(n = 2)) +
     theme(legend.position="none", 
       axis.title.x = element_blank(), 
      axis.title.y = element_blank(), 
@@ -117,18 +127,25 @@ plot_results = function(data, evaluation_measure = NULL, remove_strip_x = FALSE)
   if (remove_strip_x) {
     pl = pl + theme(strip.text.x = element_blank())
   }
-  pl
+  if (log) {
+    pl = pl + scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x, n = 2))
+  } else {
+    pl = pl + scale_y_continuous(breaks = pretty_breaks(n = 2))
+  }
   pl
 }
 
 # To show: 
-p_plaus = plot_results(res, "plausibility")
-p_plaus = plot_results(res, "log_probs")
-p_prox = plot_results(res, "proximity", remove_strip_x = TRUE)
-p_sparse = plot_results(res, "sparsity", remove_strip_x = TRUE)
-p_hv = plot_results(res_subset, "hv_normalized", remove_strip_x = TRUE)
-p_no = plot_results(res_agg, "number*", remove_strip_x = TRUE)
-p_runtime = plot_results(res_agg, "runtime*", remove_strip_x = TRUE)
+res[, plausibility := log(plausibility)]
+p_plaus = plot_results(res, "plausibility") 
+p_prox = plot_results(res, "proximity", remove_strip_x = TRUE, log = FALSE)
+p_sparse = plot_results(res, "sparsity", remove_strip_x = TRUE, log = FALSE) + 
+  scale_y_continuous(breaks = c(0.1, 0.5, 0.9))
+p_hv = plot_results(res_subset, "hypervolume", remove_strip_x = TRUE, log = TRUE) #+ 
+  #scale_y_log10(breaks = c(1e-10, 1e-5, 1, 10))
+p_no = plot_results(res_agg, "number", remove_strip_x = TRUE, log = TRUE) + 
+  scale_y_log10(breaks = c(1, 10, 100))
+p_runtime = plot_results(res_agg, "runtime", remove_strip_x = TRUE, log = TRUE)
 
 p_main = p_plaus / p_prox / p_sparse / p_hv / p_no / p_runtime
 p_main
